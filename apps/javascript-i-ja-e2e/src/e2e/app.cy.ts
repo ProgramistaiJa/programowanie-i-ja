9ca5d0076245687405e9506576336e29d1547bfd
ProgramistaiJa <programista@i-ja.pl>
2022-09-30 15:17:00 +0200

    Initial commit

---
+++ apps/javascript-i-ja-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('javascript-i-ja', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome javascript-i-ja');
+  });
+});
